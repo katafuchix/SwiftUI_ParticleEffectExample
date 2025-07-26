8de3bf9059949d7cab04db7e87a2541ff7c9a528
katafuchix <katafuchix@gmail.com>
2025-07-26 11:29:00 +0900

    Initial Commit

---
+++ SwiftUI_ParticleEffectExample/SwiftUI_ParticleEffectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ParticleEffectExampleApp.swift
+//  SwiftUI_ParticleEffectExample
+//
+//  Created by cano on 2025/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ParticleEffectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
